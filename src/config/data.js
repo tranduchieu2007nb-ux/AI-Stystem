1e8aa2a0aac277f77b3c61ae204358ff882c4d55
dat2007-hub <dat.td.2727@aptechlearning.edu.vn>
2026-07-10 09:42:50 +0700

    merge

--- src/config/data.js
+++ src/config/data.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 
 const connections = async (req, res) => {
     try {
-        await mogoose.connect(process.env.DB_HOST);
+        await mogoose.connect("mongodb+srv://tranduchieu:TranH2007tbb@cluster0.rrqomah.mongodb.net/AI_Styme");
         console.log("connect ok database");
     } catch (error) {
         console.log("error database : ", error);
